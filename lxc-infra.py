c6115d0d58baa661d491967cda58167430110e3a
root <root@infra.openstack.local>
2017-03-10 08:47:55 +0000

    update

---
+++ lxc-infra.py
@@ -0,0 +1,32 @@
+NAME                                          STATE   AUTOSTART GROUPS            IPV4                                       IPV6 
+infra_aodh_container-316286d7                 RUNNING 1         onboot, openstack 10.0.3.159, 172.29.238.20                  -    
+infra_ceilometer_api_container-ec0f8448       RUNNING 1         onboot, openstack 10.0.3.201, 172.29.238.131                 -    
+infra_ceilometer_collector_container-c1a359bd RUNNING 1         onboot, openstack 10.0.3.225, 172.29.239.117                 -    
+infra_cinder_api_container-35d8db74           RUNNING 1         onboot, openstack 10.0.3.35, 172.29.237.11, 172.29.246.60    -    
+infra_cinder_scheduler_container-b4783b54     RUNNING 1         onboot, openstack 10.0.3.182, 172.29.238.185                 -    
+infra_galera_container-17e31542               RUNNING 1         onboot, openstack 10.0.3.96, 172.29.239.247                  -    
+infra_galera_container-a85c8cd6               RUNNING 1         onboot, openstack 10.0.3.117, 172.29.238.228                 -    
+infra_galera_container-d4d573d6               RUNNING 1         onboot, openstack 10.0.3.104, 172.29.237.92                  -    
+infra_glance_container-73d70613               RUNNING 1         onboot, openstack 10.0.3.162, 172.29.238.239, 172.29.247.181 -    
+infra_gnocchi_container-2ab32f63              RUNNING 1         onboot, openstack 10.0.3.12, 172.29.239.225                  -    
+infra_heat_apis_container-7f3bd8ff            RUNNING 1         onboot, openstack 10.0.3.148, 172.29.239.135                 -    
+infra_heat_engine_container-c93c7b41          RUNNING 1         onboot, openstack 10.0.3.105, 172.29.239.19                  -    
+infra_horizon_container-2654da44              RUNNING 1         onboot, openstack 10.0.3.197, 172.29.237.3                   -    
+infra_horizon_container-bfe92d28              RUNNING 1         onboot, openstack 10.0.3.82, 172.29.238.201                  -    
+infra_keystone_container-06b0d723             RUNNING 1         onboot, openstack 10.0.3.34, 172.29.237.6                    -    
+infra_keystone_container-678798f5             RUNNING 1         onboot, openstack 10.0.3.247, 172.29.238.57                  -    
+infra_memcached_container-43232abd            RUNNING 1         onboot, openstack 10.0.3.194, 172.29.239.123                 -    
+infra_neutron_agents_container-d55f2708       RUNNING 1         onboot, openstack 10.0.3.83, 172.29.238.166, 172.29.243.177  -    
+infra_neutron_server_container-bd7e1fd6       RUNNING 1         onboot, openstack 10.0.3.41, 172.29.239.149                  -    
+infra_nova_api_metadata_container-b22b86a1    RUNNING 1         onboot, openstack 10.0.3.152, 172.29.237.184                 -    
+infra_nova_api_os_compute_container-037d1099  RUNNING 1         onboot, openstack 10.0.3.240, 172.29.238.238                 -    
+infra_nova_cert_container-0413e8c3            RUNNING 1         onboot, openstack 10.0.3.114, 172.29.238.129                 -    
+infra_nova_conductor_container-ea2d722a       RUNNING 1         onboot, openstack 10.0.3.184, 172.29.238.179                 -    
+infra_nova_console_container-d2481d50         RUNNING 1         onboot, openstack 10.0.3.199, 172.29.238.213                 -    
+infra_nova_scheduler_container-91c3b037       RUNNING 1         onboot, openstack 10.0.3.72, 172.29.239.31                   -    
+infra_rabbit_mq_container-00479ccb            RUNNING 1         onboot, openstack 10.0.3.227, 172.29.239.125                 -    
+infra_rabbit_mq_container-233c4477            RUNNING 1         onboot, openstack 10.0.3.250, 172.29.238.49                  -    
+infra_rabbit_mq_container-3176bc0c            RUNNING 1         onboot, openstack 10.0.3.122, 172.29.237.73                  -    
+infra_repo_container-8651cc83                 RUNNING 1         onboot, openstack 10.0.3.198, 172.29.238.64                  -    
+infra_rsyslog_container-34568593              RUNNING 1         onboot, openstack 10.0.3.249, 172.29.238.243                 -    
+infra_utility_container-d55e193e              RUNNING 1         onboot, openstack 10.0.3.160, 172.29.238.193                 -    
